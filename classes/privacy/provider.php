1f7adfd5c262471ea9d7be0477fdec52eb66a160
Christian Grévisse <christian.grevisse@uni.lu>
2025-11-05 23:40:47 +0100

    Fix sniff violations

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -30,12 +30,7 @@ use core_privacy\local\request\userlist;
  * @copyright  2025 Niko Hoogeveen <nikohoogeveen@catalyst-ca.net>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements
-        // This plugin does not store any data itself.
-        // It has no database tables, and it purely acts as a conduit, sending data externally.
-    \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\core_userlist_provider,
-    \core_privacy\local\request\plugin\provider {
+class provider implements \core_privacy\local\request\core_userlist_provider, \core_privacy\local\metadata\provider, \core_privacy\local\request\plugin\provider {
     /**
      * Return the fields which contain personal data.
      *
